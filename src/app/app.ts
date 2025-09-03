494775cbfed59cd40509dfb9caccd4988b410fdf
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2025-09-02 21:38:43 -0400

    intro sequence improvements

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BottomShelfComponent } from './components/bottom-shelf/bottom-shelf';
 import { TopShelfComponent } from './components/top-shelf/top-shelf';
 
@@ -9,4 +9,16 @@ import { TopShelfComponent } from './components/top-shelf/top-shelf';
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
-export class App {}
+export class App implements OnInit {
+  // Pause animations so that they don't run while the tab is inactive
+  // Primarily for the intro sequence
+  ngOnInit() {
+    document.addEventListener('visibilitychange', () => {
+      if (document.hidden) {
+        document.documentElement.classList.add('paused-animations');
+      } else {
+        document.documentElement.classList.remove('paused-animations');
+      }
+    });
+  }
+}
